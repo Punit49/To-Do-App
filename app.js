b55f6011a8472642c6fa718631f7270d16fe60d8
Punit Sahu <therealpunit5@gmail.com>
2025-11-18 20:50:15 +0530

    Added Mark as complete and other small features

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let itemArray = [];
 let itemId = JSON.parse(localStorage.getItem("itemId")) || 1;
 let isInputActive = false;
 let hoverDiv = document.querySelector(".hoverDiv");
+let completedTasks = document.querySelector(".completedTasks")
+let completedTaskArray = JSON.parse(localStorage.getItem("completedTask")) || [];
 
 // Set an item in localStorage -
 const setItem = (target, data) => {
@@ -13,16 +15,22 @@ const setItem = (target, data) => {
 
 // Accessing an item from localStorage -
 const getItem = (target) => {
-    return JSON.parse(localStorage.getItem(target));
+    return JSON.parse(localStorage.getItem(target)) || [];
 }
 
 // Window load event handling
 window.addEventListener('load', () => {
-    let items = getItem("listItems").reverse();
+    let items = getItem("listItems");
     if(items && items != ""){
         items.forEach(item => {
             let div = makeList(item.data, item.id);
-            tasks.append(div);
+            if(item.completed){
+                completedTasks.prepend(div);
+                div.querySelector(".inputheckBox").checked = true;
+            }
+            else{
+                tasks.prepend(div);
+            }
         });
     } 
 })
@@ -37,10 +45,15 @@ const generateColor = () => {
 const deleteTask = (e) => {
     let item = e.target.closest('div');
     let itemId = item.getAttribute('id');
-    tasks.removeChild(item);
     let listItems = getItem('listItems');
+    let isCompleted = listItems.filter(item => item.id == itemId);
     let updatedItems = listItems.filter(item => item.id != itemId);
     setItem("listItems", updatedItems);
+    if(isCompleted[0].completed){
+        completedTasks.removeChild(item);
+        return;
+    }
+    tasks.removeChild(item);
 }
 
 const makePopup = (event, icon) => {
@@ -59,7 +72,10 @@ const makeList = (data, id = itemId) => {
     div.style.backgroundColor = `#${generateColor()}`;
     div.innerHTML = `
         <section class="taskBox">
-            <p class="taskData">${data}</p>
+            <span>
+                <input type="checkbox" name="inputCheck" id=task${id} class="inputheckBox">
+                <label class="taskData" for=task${id}> ${data} </label>
+            </span>
             <span>
                 <span class="hoverBoxes">
                     <i class="fa-solid fa-pen editBtn iconHover" data-text="Edit Task"></i>
@@ -67,19 +83,25 @@ const makeList = (data, id = itemId) => {
                 <span class="hoverBoxes">
                     <i class="fa-solid fa-trash deleteBtn iconHover" data-text="Delete Task"></i>
                 </span>
-            </>
+            </span>
         </section>
     `;
+
     let deleteItem = div.querySelector(".deleteBtn");
-    deleteItem.addEventListener("click", deleteTask);
     let editBtn = div.querySelector('.editBtn');
     let iconHover = div.querySelectorAll('.iconHover');
+    let inputheckBox = div.querySelector('.inputheckBox');
+
+    deleteItem.addEventListener("click", deleteTask);
+
     editBtn.addEventListener('click', (e) => {
          editFunction(div, e);
     })
+
     div.addEventListener("dblclick", (e) => {
          editFunction(div, e);
     });
+
     iconHover.forEach((icon) => {
         icon.addEventListener("mouseover", (event) => {
             makePopup(event, icon);
@@ -89,6 +111,34 @@ const makeList = (data, id = itemId) => {
             hoverDiv.classList.add('notVisible');
         });
     })
+
+    inputheckBox.addEventListener('change', (event) => {
+        let parent = event.target.closest('div');
+        let listItems = getItem("listItems");
+        let taskId = parent.getAttribute('id');
+        let label = parent.querySelector(".taskData");
+        let completeItem = listItems.map((item) => {
+            if(item.id == taskId){
+                if(item.completed){
+                    label.classList.remove("lineThrough");
+                    item.completed = false;
+                }
+                else {
+                    item.completed = true;
+                    label.classList.add("lineThrough");
+                }
+            } 
+            return item;
+        });
+        setItem('listItems', completeItem);
+        if(inputheckBox.checked){
+            completedTasks.prepend(parent);
+        }
+        else{
+            tasks.prepend(parent);
+        }
+    })
+
     return div;
 }
 
@@ -139,23 +189,22 @@ const handleEdit = (input, closedtBox) => {
 
 // Edit Feature
 const editFunction = (div, e) => {
-    let closedtBox = e.target.closest('section');
-    if(closedtBox != null){
+    let closestBox = e.target.closest('section');
+    if(closestBox != null){
         isInputActive = true;
-        console.log(closedtBox);
-        closedtBox.classList.add("hidden");
+        closestBox.classList.add("hidden");
         let data = div.querySelector(".taskData");
         let input = makeInput(data.textContent);
         div.append(input);
         input.focus();
         input.addEventListener('keypress', (event) => {
             if(event.key == 'Enter' && isInputActive){
-                handleEdit(input, closedtBox);
+                handleEdit(input, closestBox);
             }
         })
         input.addEventListener('blur', (event) => {
             if(isInputActive){
-               handleEdit(input, closedtBox);
+               handleEdit(input, closestBox);
             }
         });
     }
